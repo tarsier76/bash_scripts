de0a157cb2c5e5abe2d82f62c5b32bddb8c06ca1
yoohello <100511410+yoohello@users.noreply.github.com>
2024-01-21 14:11:46 -0500

    Changed print messages

--- local_backup.sh
+++ local_backup.sh
@@ -8,14 +8,14 @@ fi
 echo -e "Choose one of the following options: \n1.Backup a home directory.\n2.Backup all log files on the system.\n3.Backup a specific folder in the system."
 read -r user_choice
 
-case $user_choice in
-  1)
+case $user_choice in  1)
     read -p "Enter the username for the home directory you want to backup: " user_name
     read -p "Enter the destination directory for the backup: " destination_dir
 
+
     if [ -d /home/$user_name ] && [ -d $destination_dir ]; then
         echo "Backing up home directory..."
-	tar -czvf "destination_dir/${user_name}_backed_up_dir_$(date +%Y-%m-%d).tar.gz" "/home/$user_name"
+	tar -czvf "$destination_dir/${user_name}_backed_up_dir_$(date +%Y-%m-%d).tar.gz" "/home/$user_name" &>/dev/null
 
 	if [ $? -eq 0 ]; then
 	    echo "Backup completed successfully. ('destination_dir/${user_name}_backed_up_dir_$(date +%Y-%m-%d).tar.gz')"
@@ -23,7 +23,7 @@ case $user_choice in
 	    echo "Error: Backup process encountered an issue."
 	fi
     else
-	echo "Error: Either username or destination directory are invalid. Try again."
+	echo "Error: Destination directory or username is invalid. Try again."
     fi
     ;;
   *)
