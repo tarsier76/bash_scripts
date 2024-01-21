ae3e14b598d0296c7301662c38774acf9c6fc0c5
yoohello <testing@gmail.com>
2024-01-21 06:36:18 -0500

    Changed backup method and added more echo statements

--- local_backup.sh
+++ local_backup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $EUID -ne 0 ]; then
+  echo "The script needs to be run with elevated privilleges. Exiting."
+  exit 1
+fi
+
 echo -e "Choose one of the following options: \n1.Backup a home directory.\n2.Backup all log files on the system.\n3.Backup a specific folder in the system."
 read -r user_choice
 
@@ -9,8 +14,14 @@ case $user_choice in
     read -p "Enter the destination directory for the backup: " destination_dir
 
     if [ -d /home/$user_name ] && [ -d $destination_dir ]; then
-    	echo "Backing up home directory..." && rsync -avz /home/$user_name/ $destination_dir/${user_name}_backed_up_dir_$(date +%Y-%m-%d) &>/dev/null
-        echo "Backup completed successfully."
+        echo "Backing up home directory..."
+	tar -czvf "destination_dir/${user_name}_backed_up_dir_$(date +%Y-%m-%d).tar.gz" "/home/$user_name"
+
+	if [ $? -eq 0 ]; then
+	    echo "Backup completed successfully. ('destination_dir/${user_name}_backed_up_dir_$(date +%Y-%m-%d).tar.gz')"
+	else
+	    echo "Error: Backup process encountered an issue."
+	fi
     else
 	echo "Error: Either username or destination directory are invalid. Try again."
     fi
