27dc011f71025f018ee4fb199ec1718a67a49a3a
yoohello <100511410+yoohello@users.noreply.github.com>
2024-02-13 05:17:45 -0500

    Displays colored text and a blinking line

---
+++ practice_scripts/colored_text.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#: Description : prints some lines of colored text
+
+echo -e "\e[1;31m This is red text \e[0m"
+echo -e "\e[1;32m This is green text \e[0m"
+echo -e "\e[1;33m This is brown text \e[0m"
+echo -e "\e[1;35m This is magenta text \e[0m"
+echo -e "\e[1;5m This is blinking text \e[0m"
